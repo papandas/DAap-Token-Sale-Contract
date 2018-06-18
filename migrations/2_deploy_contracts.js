d3444df5aae770da153ea444b83d914dde0a2cea
Papan Das <hexoindia@gmail.com>
2018-06-18 15:57:57 +0530

    Compile for deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,7 @@ var DappTokenSale = artifacts.require("./DappTokenSale.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(DappToken, 1000000).then(function(){
-    var tokenPrice = 1000000000000000000;
+    var tokenPrice = 1000000000000000;
     return deployer.deploy(DappTokenSale, DappToken.address, tokenPrice);
   });
 };
--- truffle.js
+++ truffle.js
@@ -7,6 +7,13 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      host:"localhost",
+      port:8545,
+      from: "0x2d62771cb1bbb9fc81289276014b76954a57b648",
+      network_id: 4,
+      gas: 4700000
     }
   }
 };
